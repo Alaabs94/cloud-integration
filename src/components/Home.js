e97f01bc7a238f0f89dea8d4503811f5115219df
alaabs94 <alaabs.94@gmail.com>
2022-09-23 12:51:39 +0200

    map

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyA40IumwEgN0SRJyHGfBcO2CfrfdlCbKOc",
+    googleMapsApiKey: "AIzaSyAbyYmqQCQ8slHZtlC9fkzuWWXApfyZFr4",
   });
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -11,7 +11,7 @@ export default function Home() {
 }
 
 function Map() {
-  const center = useMemo(() => ({ lat: 36.576714, lng: 10.868036 }), []); // Korba Location
+  const center = useMemo(() => ({ lat: 36.576714, lng: 10.868036 }), []); // bni khaled  Location
 
   return (
     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">

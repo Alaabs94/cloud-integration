fdc37b6e1bdcea2028ebc50461a190ff28fbc7b2
alaabs94 <alaabs.94@gmail.com>
2022-09-23 12:37:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Map</h1>
+      <Home/>
+   
+    </div>
+  );
+}
+
+export default App;
